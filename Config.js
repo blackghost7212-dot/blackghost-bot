8b1d9acb97dcd6f27ab5aa482840f21a9065a94f
blackghost7212-dot <blackghost7212@gmail.com>
2026-03-12 15:49:35 +0000

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  BOT_NAME: 'black ghost',
+  OWNER_NAME: 'Dracula Tech',
+  OWNER_NUMBER: '22372125872',
+  SESSION_DIR: './baileys',
+  NO_PREFIX: true,
+  STATUS_VIEW: true
+};
